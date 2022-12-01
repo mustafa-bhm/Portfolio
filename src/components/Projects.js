6c37f12e4a8a31d00d6aa9302f00952b72a9d7a4
mustafa <mustafa.bhm3@gmail.com>
2022-12-01 06:54:13 -0700

    update project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -37,7 +37,7 @@ function Projects() {
       description:
         "This Interview Scheduler is a SPA ( single-page-application) , that can help students create / update and delete their appointments",
       imgUrl:
-        "https://github.com/mustafa-bhm/scheduler/blob/master/docs/Screen%20Shot%20.png?raw=true",
+        "https://github.com/mustafa-bhm/scheduler/blob/master/docs/Screen%20Shot%20.png",
       liveDemo: "https://www.youtube.com/watch?v=kdaE9yyipCI",
       githubRepo: "https://github.com/mustafa-bhm/scheduler",
     },
@@ -46,7 +46,7 @@ function Projects() {
       description:
         "A mini e-commerce application built with Rails 6.1 for purposes of learning Rails",
       imgUrl:
-        "https://github.com/mustafa-bhm/jungle-rails/blob/master/docs/Screen%20Shot%202022-09-27%20at%201.49.30%20PM.png?raw=true",
+        "https://github.com/mustafa-bhm/jungle-rails/blob/master/docs/Screen%20Shot%202022-09-27%20at%201.49.30%20PM.png",
       liveDemo: "",
       githubRepo: "https://github.com/mustafa-bhm/jungle-rails",
     },
@@ -55,7 +55,7 @@ function Projects() {
       description:
         "This is a responsive , simple and single-page Twitter clone",
       imgUrl:
-        "https://github.com/mustafa-bhm/tweeter./blob/main/public/images/Desktop-2.png?raw=true",
+        "https://raw.githubusercontent.com/mustafa-bhm/tweeter./main/public/images/Desktop-2.png",
       liveDemo: "",
       githubRepo: "https://github.com/mustafa-bhm/tweeter.",
     },
@@ -63,7 +63,7 @@ function Projects() {
       title: "To-do-App ",
       description: "A To-Do-App built with HTML , CSS and Javascript",
       imgUrl:
-        "https://github.com/mustafa-bhm/To-do-list/blob/master/assets/Screen%20Shot%202022-11-30%20at%208.18.18%20PM.png?raw=true",
+        "https://raw.githubusercontent.com/mustafa-bhm/To-do-list/master/assets/Screen%20Shot%202022-11-30%20at%208.18.18%20PM.png",
       liveDemo: "https://www.google.com/",
       githubRepo: "https://github.com/mustafa-bhm/To-do-list",
     },
